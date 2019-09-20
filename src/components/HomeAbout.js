fac7a17c414aedd372f08afdb1bdd614392f5abc
Agnieszka Drewniok <agnieszka.drewniok@gmail.com>
2019-09-20 16:02:09 +0200

    About, three columns

--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -1,12 +1,21 @@
 import React, {Component} from "react";
+import { ReactComponent as Decoration } from "../assets/Decoration.svg";;
 
 class HomeAbout extends Component{
 
     render(){
         
         return(
-            <div style={{height: "300px"}}>
-                About
+            <div className="HomeAbout-container">
+                <div>
+                    <h1>O nas</h1>
+                    <Decoration/>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacus sapien, iaculis nec commodo vel, lacinia ac tortor. Praesent interdum mi neque, ut lacinia mi congue in. </p>
+                </div>
+                <div className="HomeAbout-img">
+
+                </div>
+                
             </div>
         )
     }
--- src/components/HomeSteps.js
+++ src/components/HomeSteps.js
@@ -1,12 +1,48 @@
 import React, {Component} from "react";
+import {NavLink} from "react-router-dom";
+import { ReactComponent as Decoration } from "../assets/Decoration.svg";
+import { ReactComponent as Icon1 } from "../assets/Icon-1.svg";
+import { ReactComponent as Icon2 } from "../assets/Icon-2.svg";
+import { ReactComponent as Icon3 } from "../assets/Icon-3.svg";
+import { ReactComponent as Icon4 } from "../assets/Icon-4.svg";
+
+
 
 class HomeSteps extends Component{
 
     render(){
         
         return(
-            <div style={{height: "300px"}}>
-                Steps
+            <div className="HomeSteps">
+                <h1>Wystarczą 4 proste kroki</h1>
+                <Decoration/>
+                <div className="HomeSteps-steps">
+                    <div>
+                        <Icon1/>
+                        <h1>Wybierz rzeczy</h1> 
+                        <p>______</p>
+                        <h2>ubrania, zabawki, sprzęt i inne</h2>
+                    </div>
+                    <div>
+                        <Icon2/>
+                        <h1>Wybierz rzeczy</h1> 
+                        <p>______</p>
+                        <h2>ubrania, zabawki, sprzęt i inne</h2>
+                    </div>
+                    <div>
+                        <Icon3/>
+                        <h1>Wybierz rzeczy</h1> 
+                        <p>______</p>
+                        <h2>ubrania, zabawki, sprzęt i inne</h2>
+                    </div>
+                    <div>
+                        <Icon4/>
+                        <h1>Wybierz rzeczy</h1> 
+                        <p>______</p>
+                        <h2>ubrania, zabawki, sprzęt i inne</h2>
+                    </div>
+                </div>
+                <button><NavLink to={`/logowanie`}>Oddaj rzeczy</NavLink></button>
             </div>
         )
     }
