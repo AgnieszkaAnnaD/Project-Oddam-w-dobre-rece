c65cbd9d469770109ae039798e8740a78defddf0
Agnieszka Drewniok <agnieszka.drewniok@gmail.com>
2019-09-23 17:03:41 +0200

    JSONserver

--- src/components/HomeContact.js
+++ src/components/HomeContact.js
@@ -1,4 +1,7 @@
 import React, {Component} from "react";
+import { ReactComponent as Decoration } from "../assets/Decoration.svg";
+import instagram from "../assets/Instagram.svg";
+import facebook from "../assets/Facebook.svg";
 
 class HomeContact extends Component{
 
@@ -10,17 +13,38 @@ class HomeContact extends Component{
         
         return(
             <div className="HomeContact-container">
+                
                 <div className="HomeContact-form">
+                    <div className="HomeContact-form-header">
+                        <h1>Skontaktuj się z nami</h1>
+                        <Decoration/>  
+                    </div>
                     <form>
-                        <h1>Wpisz swoje imię</h1>
-                        <input placeholder="Krzysztof"></input>
-                        <h1>Wpisz swój email</h1>
-                        <input placeholder="abc@xyz.pl"></input>
-                        <h1>Wpisz swoją wiadomość</h1>
-                        <textarea placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis sem eget metus auctor, a finibus orci sagittis. Nullam magna urna, ultrices pharetra dolor at, porttitor ultrices justo. Maecenas consequat, nunc sed scelerisque posuere"></textarea>
+                        <div className="HomeContact-form-inputs">
+                            <div className="HomeContact-form-input">
+                                <h1>Wpisz swoje imię</h1>
+                                <input placeholder="Krzysztof" type="text"></input>  
+                            </div>
+                            <div className="HomeContact-form-input">
+                                <h1>Wpisz swój email</h1>
+                                <input placeholder="abc@xyz.pl" type="email"></input>   
+                            </div>
+                        </div>
+                        <div className="HomeContact-form-textarea">
+                            <h1>Wpisz swoją wiadomość</h1>
+                            <textarea placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis sem eget metus auctor, a finibus orci sagittis. Nullam magna urna, ultrices pharetra dolor at, porttitor ultrices justo. Maecenas consequat, nunc sed scelerisque posuere"></textarea>  
+                        </div>
                         <button onSubmit={this.handleSubmit}>Wyślij</button>
                     </form>
+
                 </div>
+                <footer className="HomeContact-footer">
+                    <h1>Copyright by Coders Lab</h1>
+                    <div>
+                        <img src={instagram}/>
+                        <img src={facebook}/>
+                    </div>
+                </footer>
             </div>
         )
     }
--- src/components/HomeHelp.js
+++ src/components/HomeHelp.js
@@ -3,6 +3,56 @@ import { ReactComponent as Decoration } from "../assets/Decoration.svg";
 
 class HomeHelp extends Component{
 
+    state = {
+        foundations: [],
+        organizations: [],
+        gatherings: []
+    }
+
+    handleClickF = () => {
+        fetch(`http://localhost:3001/fundacje`)
+        .then(resp => resp.json())
+        .then(foundAPI => {
+            this.setState({
+                foundations: foundAPI,
+                choose: "foundations",
+            })
+        }) 
+        .catch(err => console.error(err));
+    }
+
+    handleClickO = () => {
+        console.log("click");
+        this.setState({
+            choose: "organizations"
+        });
+        fetch(`http://localhost:3001/organizacje`)
+        .then(resp => resp.json())
+        .then(orgAPI => {
+            this.setState({
+                organizations: orgAPI,
+            })
+        }) 
+        .catch(err => console.error(err));
+    }
+
+    handleClickZ = () => {
+        console.log("click");
+        this.setState({
+            choose: "gatherings"
+        });
+        fetch(`http://localhost:3001/zbiorki`)
+        .then(resp => resp.json())
+        .then(gathAPI => {
+            this.setState({
+                gatherings: gathAPI,
+            })
+        }) 
+        .catch(err => console.error(err));
+    }
+
+
+
     render(){
         
         return(
@@ -10,9 +60,9 @@ class HomeHelp extends Component{
                 <h1>Komu pomagamy?</h1>
                 <Decoration/>
                 <div>
-                    <button>Fundacjom</button>
-                    <button>Organizacjom pozarządowym</button>
-                    <button>Lokalnym zbiórkom</button>
+                    <button onClick={this.handleClickF}>Fundacjom</button>
+                    <button onClick={this.handleClickO}>Organizacjom pozarządowym</button>
+                    <button onClick={this.handleClickZ}>Lokalnym zbiórkom</button>
                 </div>
             </div>
         )
