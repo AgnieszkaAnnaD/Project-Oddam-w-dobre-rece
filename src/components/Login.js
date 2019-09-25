96a5277203be6887c588e4ae637f5b5030ed4f52
Agnieszka Drewniok <agnieszka.drewniok@gmail.com>
2019-09-25 03:17:42 +0200

    Login and register layout

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,57 @@
 import React, {Component} from "react";
+import { ReactComponent as Decoration } from "../assets/Decoration.svg";
+import {NavLink} from "react-router-dom";
+
 
 class Login extends Component{
 
     render(){
+
+        
         
         return(
-            <div style={{height: "300px"}}>
-                Login
+            <>
+            <div className="HomeHeader-log">
+               <ul>
+                   <li>
+                       <NavLink to={`/logowanie`}>Zaloguj</NavLink>
+                    </li>
+                   <li>
+                       <NavLink to={`/rejestracja`}>Załóż konto</NavLink>
+                    </li>
+               </ul>
             </div>
+            <div className="HomeHeader-nav">
+                <ul>
+                   <li>Start</li>
+                   <li>O co chodzi?</li>
+                   <li>O nas</li>
+                   <li>Fundacja i organizacje</li>
+                   <li>Kontakt</li>
+               </ul>
+            </div>
+
+            <div className="Login-container">
+                <div>
+                    <h1>Zaloguj się</h1>
+                    <Decoration/>
+                    <div className="Login-form">
+                        <h2>Email</h2>
+                        <input></input>
+                        <h2>Hasło</h2>
+                        <input></input>
+                    </div>
+                </div> 
+                <ul className="Login-links">
+                   <li>
+                       <NavLink to={`/rejestracja`}>Załóż konto</NavLink>
+                    </li>
+                   <li>
+                       <NavLink to={`/logowanie`}>Zaloguj się</NavLink>
+                    </li>
+               </ul>     
+            </div>
+            </>
         )
     }
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,56 @@
 import React, {Component} from "react";
+import { ReactComponent as Decoration } from "../assets/Decoration.svg";
+import {NavLink} from "react-router-dom";
 
 class Register extends Component{
 
     render(){
         
         return(
-            <div style={{height: "300px"}}>
-                Rejestracja
+            <>
+            <div className="HomeHeader-log">
+               <ul>
+                   <li>
+                       <NavLink to={`/logowanie`}>Zaloguj</NavLink>
+                    </li>
+                   <li>
+                       <NavLink to={`/rejestracja`}>Załóż konto</NavLink>
+                    </li>
+               </ul>
             </div>
+            <div className="HomeHeader-nav">
+                <ul>
+                   <li>Start</li>
+                   <li>O co chodzi?</li>
+                   <li>O nas</li>
+                   <li>Fundacja i organizacje</li>
+                   <li>Kontakt</li>
+               </ul>
+            </div>
+
+            <div className="Login-container">
+                <div>
+                    <h1>Zaloguj się</h1>
+                    <Decoration/>
+                    <div className="Register-form">
+                        <h2>Email</h2>
+                        <input></input>
+                        <h2>Hasło</h2>
+                        <input></input>
+                        <h2>Powtórz hasło</h2>
+                        <input></input>
+                    </div>
+                </div> 
+                <ul className="Login-links">
+                   <li>
+                       <NavLink to={`/rejestracja`}>Załóż konto</NavLink>
+                    </li>
+                   <li>
+                       <NavLink to={`/logowanie`}>Zaloguj się</NavLink>
+                    </li>
+               </ul>     
+            </div>
+            </>
         )
     }
 }
